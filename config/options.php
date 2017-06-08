d8b1542197466c25e8d24a669b75cbecccabb28b
Anna Iosif <hej@annaiosif.se>
2017-06-08 22:08:12 +0200

    checking x-hub-signature

--- config/options.php
+++ config/options.php
@@ -11,7 +11,8 @@
         'facebook' => [
             'facebook_access_token' => env('FACEBOOK_ACCESS_TOKEN'),
             'facebook_api_url' => env('FACEBOOK_API_URL'),
-            'facebook_verify_token' => env('FACEBOOK_VERIFY_TOKEN')
+            'facebook_verify_token' => env('FACEBOOK_VERIFY_TOKEN'),
+            'facebook_secret' => env('FACEBOOK_SECRET')
         ]
     ];
     
