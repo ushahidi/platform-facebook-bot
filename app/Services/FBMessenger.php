9e200b7b46c658d7073c1131faec46d8fe2ee828
Anna Iosif <hej@annaiosif.se>
2017-06-16 16:00:28 +0200

    updating services with new userflow

--- app/Services/FBMessenger.php
+++ app/Services/FBMessenger.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use App\Models\Report;
 use App\Models\Answer;
+use GuzzleHttp\Exception\ClientException;
 
 class FBMessenger
 {
@@ -16,10 +17,18 @@ class FBMessenger
     const PAYLOAD = 'entry.0.messaging.0.postback.payload';
     const LOCATION = 'entry.0.messaging.0.message.attachments.0.payload.coordinates';
     const URL = 'entry.0.messaging.0.message.attachments.0.payload.url';
+    const MAKE_REPORT = 'make report';
+    const IMAGE_LOCATION = 'image location';
+    const LOCATION_RECEIVED = 'location received';
+    const IMAGE_RECEIVED = 'image received';
+    const ADD_IMAGE = 'add image';
+    const ADD_LOCATION = 'add location';
+    const SEND = 'send';
+    const EDIT = 'submit';
 
     public function __construct($config) {
         $this->config = $config;
-        $this->platform = new PlatformApiService(config('options.ushahidi'));
+        $this->platform = new PlatformApiService($this->config['ushahidi']);
     }
 
     /**
@@ -28,7 +37,7 @@ class FBMessenger
     **/
     public function verifyWebhook()
     {
-        if(isset($_GET[self::HUB_CHALLENGE]) && $_GET[self::HUB_VERIFY_TOKEN] === $this->config['facebook_verify_token']) {
+        if(isset($_GET[self::HUB_CHALLENGE]) && $_GET[self::HUB_VERIFY_TOKEN] === $this->config['facebook']['facebook_verify_token']) {
             return $_GET[self::HUB_CHALLENGE];
         }
         return response('Forbidden', 403);
@@ -40,149 +49,189 @@ class FBMessenger
     **/
     public function startConversation(Request $request) 
     {
-        $location = null;
         // When the user send a written-message
         if($request->input(self::MESSAGE)) {
             $message = $request->input(self::MESSAGE);
+            $message_type = 'text';
         }
         // When the user selects a button
         else if($request->input(self::PAYLOAD)) {
             $message = $request->input(self::PAYLOAD);
+            $message_type = 'payload';
         }
         // When the user adds media
         else if($request->input(self::URL)){
             $message = $request->input(self::URL);
+            $message_type = 'url';
         } 
         // When the user shares location
         else if($request->input(self::LOCATION)) {
-            $message = 'location';
-            $location = $request->input(self::LOCATION);
+            $message_type = 'location';
+            $message = $request->input(self::LOCATION);
         }
         // When the user makes a response we have not configured
         else {
+            $message_type = 'text';
             $message = 'not readable';
         }
         // the user_id we will reply to
         $recipient = $request->input(self::SENDER_ID);
+
         // the message id
         $messageId = $request->input(self::MESSAGE_ID);
-        // if the user have an ongoing report.
-        $ongoingReport = Report::where('user_id', $recipient)
-            ->first();
-
+        
+        // if the user have an ongoing report, we fetch it from database.
+        $ongoingReport = Report::where('user_id', $recipient)->first();
         if($recipient !== $messageId) {
-            $message = strtolower($message);
-            $answer = Answer::where('command', '=', $message)->first();
-            if($answer) {
-                $answer = json_decode($answer->answer);
-                // always deleting an ongoing report if user choose to click on an old button/give another command than answering the question, that indicates user wants out of the report...
+            // if $message is a location-array, strtolower is not working...
+            if($message_type === 'location') {
+                $command = self::LOCATION_RECEIVED;
+            } else {
+                $command = strtolower($message);
+        }
+
+        // getting the bot-answer from database 
+            $answers = Answer::where('command', '=', $command)->first();
+          if(!empty($answers) && $message_type !== 'location') {
+            // creates a start-report
+            if($message === self::MAKE_REPORT) {
                 if($ongoingReport) {
                     $ongoingReport->delete();
                 }
-                if($message === 'start') {
-                // creates a start-report
-                    Report::create(['user_id' => $recipient, 'last_question' => 'title']);
+
+                // fetching attributes from platform
+                $attributes = $this->platform->getAttributes();
+                
+                // initialises report in database
+                Report::create(['user_id' => $recipient, 'attributes'=> json_encode($attributes), 'replies'=> json_encode(['content' => ' ']), 'last_question' => self::MAKE_REPORT]);
+            } else if($message === self::SEND || $message === 'send my report') {
+                $this->reportingFlow($ongoingReport, $message, $recipient, $message_type);
+            } else if($message !== self::ADD_IMAGE && $message !== self::LOCATION_RECEIVED && $message !== self::IMAGE_RECEIVED && $message !== self::IMAGE_LOCATION && $message !== self::ADD_LOCATION) {
+                // deletes $ongoingReport if user do something that is not connected to the reporting-flow. This prevents the bot to get stuck in a wierd state
+                if($ongoingReport) {
+                    $ongoingReport->delete();
                 }
-                // sending reply to user
-                $this->sendMessage($answer, $recipient);    
-            } else if($ongoingReport) {
-                // starts the reporting-flow
-                $this->startReporting($ongoingReport, $message, $recipient, $location);
-            } else {
+            }
+            // sends replies
+            $this->sendMessage(unserialize($answers->answers), $recipient);
+          } else if($ongoingReport) {
+            // starts the reporting flow if there is an ongoing report
+            $this->reportingFlow($ongoingReport, $message, $recipient, $message_type);
+          } else if($message_type === 'url') {
+            // if the user sends an image, we initialise a report
+                $attributes = $this->platform->getAttributes();
+                Report::create(['user_id' => $recipient, 'attributes'=> json_encode($attributes), 'replies'=> json_encode(['content' => ' ', 'image'=> $message]), 'last_question' => self::MAKE_REPORT]);
+
+                $answers = Answer::where('command', '=', 'continue')->first();
+                
+                $this->sendMessage(unserialize($answers->answers), $recipient);               
+          } else {
                 // if the user writes something that does not makes sense
                 if($ongoingReport) {
                     $ongoingReport->delete();
                 }
-                $answer = Answer::where('command', '=', 'not readable')->first();
-                $this->sendMessage(json_decode($answer->answer), $recipient);
-            }
+                $answers = Answer::where('command', '=', 'not readable')->first();
+                // sends replies
+                $this->sendMessage(unserialize($answers->answers), $recipient);
         }
-        return response('Accepted', 200);
-
     }
+    return response('Accepted', 200);
+}
 
     /**
      * Send reply to facebook-user .
      * @param  string $recipient This is the facebook user-id 
-     * @param  array $reply This is the formatted message from Answers-table, to send to * fb-api 
+     * @param  array $answer This is the formatted messages from Answers-table, to send to * fb-api 
      */
-    private function sendMessage($reply, $recipient)
+    private function sendMessage($answers, $recipient)
     {
-        $client = new Client();
-        $header = ['Accept' => 'application/json'];
-        $query = [
-            'access_token' => $this -> config['facebook_access_token'],
-            'recipient' => ['id' => $recipient],
-            'message' => $reply
-        ];
+        foreach($answers as $answer){
+            $client = new Client();
+            $header = ['Accept' => 'application/json'];
+            $query = [
+                'access_token' => $this->config['facebook']['facebook_access_token'],
+                'recipient' => ['id' => $recipient],
+                'message' => $answer
+            ];
 
-        // todo: Catch error-responses
-        $response = $client -> request('POST', $this->config['facebook_api_url'], [
-            'headers' => $header,
-            'query' => $query
-        ]);
+            // todo: Catch error-responses
+            try {
+            $client->request('POST', $this->config['facebook']['facebook_api_url'], [
+                'headers' => $header,
+                'query' => $query
+            ]);
+            } catch (ClientException $e) {
+                \Log::ERROR('error when sending message to facebook: ' . $e->getMessage());
+            }   
+        }
     }
-            
+           
     /* Start collecting reports */
-    private function startReporting($ongoingReport, $message, $recipient, $location)
+    private function reportingFlow($ongoingReport, $message, $recipient, $message_type)
     {
-       // todo: figure out something more clean for below code and fetch attributes from platform-api instead of hardcoding them... 
-        switch ($ongoingReport->last_question) {
-            case 'title':
-                $ongoingReport->replies = json_encode([
-                    'title' => $message
-                ]);
-                $command = 'description';
-                $ongoingReport->last_question = $command;
-                $ongoingReport->save();
-                break;
-            case 'description':
-                $replies = json_decode($ongoingReport->replies);
-                $replies->content = $message;
+        $replies = json_decode($ongoingReport->replies);
+        if($message === self::SEND || $message === 'Send my report') {
+            // saving the report to the platform
+            $nextQuestion = $this->platform->savePost($ongoingReport);
+            if($nextQuestion !== 'platform error') {
+            // deletes the ongoing report
+                $ongoingReport->delete();
+            }
+        } else if($ongoingReport->last_question === self::MAKE_REPORT) {
+            // saves the reporttext
+            $replies->content = $message;
+            $ongoingReport->replies = json_encode($replies);
+            $ongoingReport->last_question = self::IMAGE_LOCATION;
+            if(!isset($replies->image)){
+                $nextQuestion = self::IMAGE_LOCATION;
+            } else {
+                $nextQuestion = self::IMAGE_RECEIVED;
+            }
+            $ongoingReport->save();
+        } else if($ongoingReport->last_question === self::IMAGE_LOCATION){
+            if($message_type === 'text') {
+                // if the user sends multiple texts, we add them to the report-text
+                $replies->content = $replies->content . ' ' . $message;
                 $ongoingReport->replies = json_encode($replies);
-                $command = 'image';
-                $ongoingReport->last_question = $command;
                 $ongoingReport->save();
-                break;
-            case 'image':
-                //check if image-url, else send another question.
-                $replies = json_decode($ongoingReport->replies);
+                $nextQuestion = self::IMAGE_LOCATION;
+            } else if($message_type === 'url') {
+                // saving image
                 $replies->image = $message;
                 $ongoingReport->replies = json_encode($replies);
-                $command = 'find_location';
-                $ongoingReport->last_question = 'location';
                 $ongoingReport->save();
-                break;
-            case 'location':
-                if(isset($location)) {
-                //extracting lat/long if user shared location
-                $replies = json_decode($ongoingReport->replies);
-                $replies->location = [
-                    'lat'=>$location['lat'],
-                    'lon'=>$location['long']
-                    ];
-                $ongoingReport->replies = json_encode($replies);
-                }
-                $command = 'finished';
-                $ongoingReport->last_question = $command;
-                $ongoingReport->save();
-                break;
-            case 'finished':
-            // if the user clicks exit, bot will delete the ongoing report and start conversation-loop from the beginning again
-                 if($message === 'send') {
-                      $command = $this->platform->savePost($ongoingReport->replies);
+                if(!isset($replies->location)) {
+                    // when the user has shared an image but there is no location saved
+                    $nextQuestion = self::IMAGE_RECEIVED;    
                 } else {
-                    $command = 'not readable';
+                    // if user has saved location and image, we ask if they want to save report to platform
+                   $nextQuestion = 'send report';
+                    $ongoingReport->delete();
                 }
-                break;
-            default:
-            // if the user do something that the bot does not understand
-            $command = 'not readable';
-            break;
-        }
-        // getting the answer from db based on value of $command
-        $answer = Answer::where('command', '=', $command)->first();
-        $answer = json_decode($answer->answer);
-        $this->sendMessage($answer, $recipient);
+            } else if($message_type === 'location') {
+                // saves the location
+                $replies->location = ['lat' => $message['lat'], 'lon' => $message['long']];
+                $ongoingReport->replies = json_encode($replies);
+                $ongoingReport->save();
+                if(!isset($replies->image)) {
+                    // when the user has shared location but there is no image saved
+                    $nextQuestion = self::LOCATION_RECEIVED;    
+                } else {
+                    // if the user has saved location and image, we ask if they want to save report to platform
+                    $nextQuestion = 'send report';
+                }
+            } else if($message_type='payload') {
+                // saving report to platform
+                $nextQuestion = $this->platform->savePost($ongoingReport);
+                $ongoingReport->delete();
+            }
+        } 
+         else {
+            // if the user does something that does not makes sense
+            $nextQuestion = 'not readable';
+        } 
+        $answers = Answer::where('command', '=', $nextQuestion)->first();
+        $this->sendMessage(unserialize($answers->answers), $recipient);
+        return response('Accepted', 200);
     }
 }
