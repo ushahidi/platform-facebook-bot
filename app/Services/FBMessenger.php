a9d02e14c50d8c7d5135f787a54ce9e2b4072b8e
Anna Iosif <hej@annaiosif.se>
2017-06-23 19:45:57 +0100

    not deleting report when adding an image

--- app/Services/FBMessenger.php
+++ app/Services/FBMessenger.php
@@ -89,12 +89,11 @@ class FBMessenger
             } else {
                 $command = strtolower($message);
         }
-
         // getting the bot-answer from database 
             $answers = Answer::where('command', '=', $command)->first();
           if(!empty($answers) && $message_type !== 'location') {
             // creates a start-report
-            if($message === self::MAKE_REPORT) {
+            if($command === self::MAKE_REPORT) {
                 if($ongoingReport) {
                     $ongoingReport->delete();
                 }
@@ -104,9 +103,9 @@ class FBMessenger
                 
                 // initialises report in database
                 Report::create(['user_id' => $recipient, 'attributes'=> json_encode($attributes), 'replies'=> json_encode(['content' => ' ']), 'last_question' => self::MAKE_REPORT]);
-            } else if($message === self::SEND || $message === 'send my report') {
+            } else if($command === self::SEND || $command === 'send my report') {
                 $this->reportingFlow($ongoingReport, $message, $recipient, $message_type);
-            } else if($message !== self::ADD_IMAGE && $message !== self::LOCATION_RECEIVED && $message !== self::IMAGE_RECEIVED && $message !== self::IMAGE_LOCATION && $message !== self::ADD_LOCATION) {
+            } else if($command !== self::ADD_IMAGE && $command !== self::LOCATION_RECEIVED && $command !== self::IMAGE_RECEIVED && $command !== self::IMAGE_LOCATION && $command !== self::ADD_IMAGE && $command !== self::ADD_LOCATION) {
                 // deletes $ongoingReport if user do something that is not connected to the reporting-flow. This prevents the bot to get stuck in a wierd state
                 if($ongoingReport) {
                     $ongoingReport->delete();
@@ -205,8 +204,7 @@ class FBMessenger
                     $nextQuestion = self::IMAGE_RECEIVED;    
                 } else {
                     // if user has saved location and image, we ask if they want to save report to platform
-                   $nextQuestion = 'send report';
-                    $ongoingReport->delete();
+                    $nextQuestion = 'send report';
                 }
             } else if($message_type === 'location') {
                 // saves the location
